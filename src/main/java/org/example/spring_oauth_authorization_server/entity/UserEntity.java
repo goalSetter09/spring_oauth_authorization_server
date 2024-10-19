70693e46e05921f3acf1f79e7d156b912b872392
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-10-19 19:04:06 +0900

    refactor: UserEntity 제약조건 추가 및 빌더 생성

--- src/main/java/org/example/spring_oauth_authorization_server/entity/UserEntity.java
+++ src/main/java/org/example/spring_oauth_authorization_server/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package org.example.spring_oauth_authorization_server.entity;
 
+import org.antlr.v4.runtime.misc.NotNull;
 import org.example.spring_oauth_authorization_server.enums.UserRole;
 
 import jakarta.persistence.Entity;
@@ -8,22 +9,46 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_entity")
 public class UserEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotNull
 	private String username;
+
+	@NotNull
 	private String password;
+
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private UserRole role;
+
+	@NotNull
 	private String nickname;
+
+	@NotNull
 	private String phone;
+
+	@Builder
+	public UserEntity(String username, String password, UserRole role, String nickname, String phone) {
+
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.nickname = nickname;
+		this.phone = phone;
+	}
+
 }
