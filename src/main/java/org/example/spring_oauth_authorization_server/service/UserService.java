32da87e324207871a5515c3be1340e2232f1c759
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-10-19 17:07:02 +0900

    feat: 기본 회원가입 로직 작성

---
+++ src/main/java/org/example/spring_oauth_authorization_server/service/UserService.java
@@ -0,0 +1,32 @@
+package org.example.spring_oauth_authorization_server.service;
+
+import org.example.spring_oauth_authorization_server.dto.UserJoinRequest;
+import org.example.spring_oauth_authorization_server.entity.UserEntity;
+import org.example.spring_oauth_authorization_server.enums.UserRole;
+import org.example.spring_oauth_authorization_server.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public void JoinUser(UserJoinRequest userJoinRequest) {
+		UserEntity userEntity = UserEntity.builder()
+			.username(userJoinRequest.username())
+			.password(bCryptPasswordEncoder.encode(userJoinRequest.password()))
+			.nickname(userJoinRequest.nickname())
+			.phone(userJoinRequest.phone())
+			.role(UserRole.ROLE_ADMIN)
+			.build();
+
+		userRepository.save(userEntity);
+	}
+}
