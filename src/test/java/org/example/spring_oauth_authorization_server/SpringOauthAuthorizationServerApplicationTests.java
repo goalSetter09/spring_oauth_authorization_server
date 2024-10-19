0716b58e1442cccdb8f28b7ef217ca86fe21e355
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-10-19 15:53:49 +0900

    initial commit

---
+++ src/test/java/org/example/spring_oauth_authorization_server/SpringOauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_oauth_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
